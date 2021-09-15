23935612a31119034fd4c19e83c5eece4e376253
Cooper <cooperflynn@utexas.edu>
2021-09-15 15:35:18 -0500

    add inquirer functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 const consoleTable = require('console.table');
+const { allowedNodeEnvironmentFlags } = require('process');
 
 // Create connection to database
 const connection = mysql.createConnection ({
@@ -13,7 +14,61 @@ const connection = mysql.createConnection ({
 connection.connect((err => {
     if (err) throw err;
     console.log('Successfully connected to database!');
-}))
+    promptUser();
+}));
+
+const promptUser = () => {
+    inquirer.prompt({
+        type: "list",
+        name: "task",
+        message: "What would you like to do?",
+        choices: [
+            'View Employees',
+            'View Roles',
+            'View Departments',
+            'Add Employee',
+            'Add Role',
+            'Add Department',
+            'Update Employee Role',
+            'Exit'
+        ]
+    })
+    .then(function ({ task }) {
+        switch (task) {
+            case 'View Employees':
+                queryTable('employee', 'SELECT * FROM employee');
+                break;
+            case 'View Departments':
+                queryTable('department', `SELECT * FROM department`);
+            case 'View Roles':
+                queryTable("role", `SELECT * FROM role`);
+            case 'Add Employee':
+                addEmployee();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Update Employee Role':
+                updateEmployee();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+        }
+    })
+}
+
+async function queryTable(title, sql) {
+    const [rows, fields] = await connection.execute(sql);
+    console.table(rows);
+    promptUser();
+};
+
+
+
 
 connection.query('SELECT * FROM mytable', (err, result, feilds)=> {
     if(err) return console.error(err)
---
+++ utils/inquirerFunctions.js
@@ -0,0 +1,208 @@
+const inquirer = require('inquirer');
+
+function firstQuestion() {
+    return new Promise((res, rej) => {
+        inquirer.prompt([
+        {
+            type: 'list',
+            name: 'choice',
+            message: 'Select one:',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Add department', 'Add role','Add employee', 'Update employee role', 'View employees by manager', 'View employees by department', 'Delete employee']
+        }
+    ]).then(answer => res(answer)).catch(err => rej(err));
+    });
+};
+
+function addDepartmentQuestions() {
+    return new Promise((res, rej) => {
+        inquirer.prompt([
+            {
+            type: 'input',
+            name: 'department',
+            message: 'Enter new department name:',
+            validate: answer => {
+                if(answer){
+                    return true;
+                } else {
+                    console.log('Enter department name!');
+                    return false;
+                }
+            }
+        }
+    ]).then(answer => res(answer)).catch(err => rej(err));
+    });
+};
+
+function addRoleQuestions() {
+    return new Promise((res, rej) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Enter job title:',
+                validate: answer => {
+                    if(answer){
+                        return true;
+                    } else {
+                        console.log('Enter job title!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'Enter salary(ex: 30000.00)',
+                validate: answer => {
+                    if(answer){
+                        return true;
+                    } else {
+                        console.log('Enter salary!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'department',
+                message: 'Enter department id:',
+                validate: answer => {
+                    if(answer){
+                        return true;
+                    } else {
+                        console.log('Enter department id!');
+                        return false;
+                    }
+                }
+            }
+        ]).then(answers => res(answers)).catch(err => rej(err));
+    });
+};
+
+function addEmployeeQuestions() {
+    return new Promise((res, rej) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'firstName',
+                message: 'Enter first name:',
+                validate: answer => {
+                    if(answer){
+                        return true;
+                    } else {
+                        console.log('Enter first name!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'lastName',
+                message: 'Enter last name:',
+                validate: answer => {
+                    if(answer){
+                        return true;
+                    } else {
+                        console.log('Enter last name!');
+                        return false;
+                    }
+                }  
+            },
+            {
+                type: 'input',
+                name: 'role',
+                message: 'Enter role id:',
+                validate: answer => {
+                    if(answer){
+                        return true;
+                    } else {
+                        console.log('Enter role ID!');
+                        return false;
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'manager',
+                message: 'Enter managers id:',
+                validate: answer => {
+                    if(answer){
+                        return true;
+                    } else {
+                        console.log('Enter managers ID!');
+                        return false;
+                    }
+                }
+            }
+        ]).then(answers => res(answers)).catch(err => rej(err));
+    });
+};
+
+function updateRoleQuestions() {
+    return new Promise((res,rej) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Enter employee id:'
+            },
+            {
+                type: 'input',
+                name: 'role',
+                message: 'Enter new role:'
+            },
+            {
+                type: 'input',
+                name: 'manager',
+                message: "Enter employee's new manager"
+            }
+        ]).then(answers => res(answers)).catch(err => rej(err));
+    });
+};
+
+function viewByManagerQuestions() {
+    return new Promise((res, rej) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'id',
+                nessage: 'Enter manager id:'
+            }
+        ]).then(answer => res(answer)).catch(err => rej(err));
+    })
+};
+
+function viewByDepartmentQuestions() {
+    return new Promise((res, rej) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Enter department id:'
+            }
+        ]).then(answer => res(answer)).catch(err => rej(err));
+    })
+};
+
+function deleteEmployeeQuestions(arr) {
+    return new Promise((res, rej) => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Enter employee id:'
+            }
+        ]).then(answers => res(answers)).catch(err => rej(err));
+    })
+};
+
+module.exports = {
+    firstQuestion,
+    addDepartmentQuestions,
+    addRoleQuestions,
+    addEmployeeQuestions,
+    updateRoleQuestions,
+    viewByManagerQuestions,
+    viewByDepartmentQuestions,
+    deleteEmployeeQuestions
+};
